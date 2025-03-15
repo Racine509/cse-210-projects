2a0c430d69d91de2576b9f51dcf6dacc43e23abc
Racine509 <RACINEYARELAY@GMAIL.COM>
2025-03-14 23:57:27 -0400

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        random randomGenrarator = new random();   
+        int randomNumber = randomGenrarator.next(1,100) ;
+          int guess = 0 
+          bool hasGuessed = false;
+          console.Write("Guess a number betwenn 1 and 100");
+           while(!hasGuessed)
+           {
+            console.Write("what is the magic number?");
+            if (int .tryParse(console.readline(),out guess);)
+           }
+            if (guess > randomNumber)}
+} consle.WriteLine("Higher");
+
+esle if(guess < randomNumber
+{
+    consle.WriteLine("lower");
 }
+else
+{
+    consle.writeling("you guesses it");
+    hasGuessed = true;
+}
+else
+{
+    console.writeling("try again")
+}
+
+
