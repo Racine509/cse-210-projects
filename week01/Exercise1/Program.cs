538f6979c32bd39b2641533f396fcd42627a464c
Racine509 <RACINEYARELAY@GMAIL.COM>
2025-03-14 23:54:26 -0400

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,13 @@
+
 using System;
 
 class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+
+ static void Main(string[] args)
+ {
+    Console.Write("what is your first name.");
+    string firstname = console.readline();
+    console.Write("what is your last name");
+    string lastname = console.readline();
+    console.WriteLine = ($"your name is {firstname}, {firstname} {lastname}")
 }
