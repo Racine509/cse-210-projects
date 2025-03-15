80912b3e30ec3762613a26a1d3c798d7c51c93bb
Racine509 <RACINEYARELAY@GMAIL.COM>
2025-03-15 00:16:19 -0400

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,13 +1,15 @@
-
 using System;
 
 class Program
-
- static void Main(string[] args)
- {
-    Console.Write("what is your first name.");
-    string firstname = console.readline();
-    console.Write("what is your last name");
-    string lastname = console.readline();
-    console.WriteLine = ($"your name is {firstname}, {firstname} {lastname}")
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name: ");
+        string firstname = Console.ReadLine();
+        
+        Console.Write("What is your last name: ");
+        string lastname = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {firstname}, {firstname} {lastname}");
+    }
 }
