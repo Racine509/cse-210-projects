a7007f29f0f1c472a51a5198748c26d7eb247623
Racine509 <RACINEYARELAY@GMAIL.COM>
2025-03-15 00:20:31 -0400

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,31 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        random randomGenrarator = new random();   
-        int randomNumber = randomGenrarator.next(1,100) ;
-          int guess = 0 
-          bool hasGuessed = false;
-          console.Write("Guess a number betwenn 1 and 100");
-           while(!hasGuessed)
-           {
-            console.Write("what is the magic number?");
-            if (int .tryParse(console.readline(),out guess);)
-           }
-            if (guess > randomNumber)}
-} consle.WriteLine("Higher");
+        // Générer un nombre aléatoire entre 1 et 100
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
 
-esle if(guess < randomNumber
-{
-    consle.WriteLine("lower");
-}
-else
-{
-    consle.writeling("you guesses it");
-    hasGuessed = true;
-}
-else
-{
-    console.writeling("try again")
-}
+        int guess = 0;
+        bool hasGuessed = false;
 
+        Console.WriteLine("Guess a number between 1 and 100");
 
+        while (!hasGuessed)
+        {
+            Console.Write("What is your guess? ");
+            
+            // Vérification de la validité de l'entrée utilisateur
+            if (int.TryParse(Console.ReadLine(), out guess))
+            {
+                // Comparaison avec le nombre magique
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    hasGuessed = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
