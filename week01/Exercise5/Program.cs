bf47cfbcae34a8a9dc7f69d3f551c6aac685039d
Racine509 <RACINEYARELAY@GMAIL.COM>
2025-03-14 23:58:49 -0400

    Update Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,40 @@
 using System;
 
-class Program
+public static class Program
 {
-    static void Main(string[] args)
+    public static void DisplayWelcome()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    public static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    public static int PromptFavoriteNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    public static void DisplayResult(string name, int number, int squaredNumber)
+    {
+        Console.WriteLine($"Thank you {name}, the square of your favorite number ({number}) is {squaredNumber}.");
+    }
+
+    public static void Main()
+    {
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptFavoriteNumber();
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(name, number, squaredNumber);
     }
 }
